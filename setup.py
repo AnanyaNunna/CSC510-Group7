028c60fa5630cad26583ead5b3fe3d48d42ee201
vijayanand500 <69767520+vijayanand500@users.noreply.github.com>
2020-08-16 19:10:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='CSC510-Group7',
       version='1.0',
       description='Sample Package',
       author='vanand, draghun, anunna, ssdeshp8, abisht',
       author_email='vanand@ncsu.edu',
-      packages = setuptools.find_packages() ,
+      packages = find_packages() ,
       long_description=open('README.txt').read(),
      )
