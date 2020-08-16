fbb723482fe213b54156b7e47545655de5091aaf
vijayanand500 <69767520+vijayanand500@users.noreply.github.com>
2020-08-16 19:03:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='CSC510-Group7',
+      version='1.0',
+      description='',
+      author='vanand, draghun, anunna, ssdeshp8, abisht',
+      author_email='vanand@ncsu.edu',
+      packages=['code'],
+      long_description=open('README.txt').read(),
+     )
