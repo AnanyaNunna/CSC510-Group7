bf052809827a7126aba7ac04b38c3538ffcc9dda
vijayanand500 <69767520+vijayanand500@users.noreply.github.com>
2020-08-16 19:04:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='CSC510-Group7',
       version='1.0',
-      description='',
+      description='Sample Package',
       author='vanand, draghun, anunna, ssdeshp8, abisht',
       author_email='vanand@ncsu.edu',
       packages=['code'],
